aacae7f5ee244582e31aecea15c27999a41f2e4d
hishambokhari <hish.b92@gmail.com>
2020-07-07 13:22:13 +0500

    show message from stateon page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    message: 'This message is from the state'
+  }
   render(){
     return (
       <div className="App">
-        <h1>Hello </h1>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
