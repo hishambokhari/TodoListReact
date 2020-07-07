a55f3523ac4785cf559ccda3d6d67fcf5367d0fc
hishambokhari <hish.b92@gmail.com>
2020-07-07 13:44:12 +0500

    Listen for when the input changes and update state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 
 class App extends Component {
   state = {
-    message: 'This message is from the state'
+    message: 'This message is from the state',
+    newTodo: ''
   }
 
   formSubmitted(event) {
     event.preventDefault();
 
-    console.log('form submitted');
+    console.log(this.state.newTodo);
+  }
+
+  newTodoChanged(event) {
+    this.setState({newTodo: event.target.value})
   }
   
   render(){
@@ -18,9 +23,9 @@ class App extends Component {
     return (
       <div className="App">
         <h3>{this.state.message}</h3>
-        <form onSubmit={this.formSubmitted}>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
           <label htmlFor="newTodo">New Todo</label>
-          <input id="newTodo" name="newTodo" />
+          <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo" />
           <button type="submit">Add Todo</button>
           
         </form>
