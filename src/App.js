09828f4edb750836ecf7b5aa61fdfd9041715e56
hishambokhari <hish.b92@gmail.com>
2020-07-07 13:34:34 +0500

    Call function when form is submitted

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ class App extends Component {
   state = {
     message: 'This message is from the state'
   }
+
+  formSubmitted(event) {
+    event.preventDefault();
+
+    console.log('form submitted');
+  }
+  
   render(){
+
+  
     return (
       <div className="App">
         <h3>{this.state.message}</h3>
-        <form>
+        <form onSubmit={this.formSubmitted}>
           <label htmlFor="newTodo">New Todo</label>
           <input id="newTodo" name="newTodo" />
+          <button type="submit">Add Todo</button>
+          
         </form>
       </div>
     );
